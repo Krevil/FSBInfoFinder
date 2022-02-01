0f52d66d2ac068a49cd0a2cf1ad44d52395bb032
Krevil <krevil.3000@gmail.com>
2022-02-01 02:31:37 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FSBInfoReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = new FileStream(args[0], FileMode.Open);
+            uint InfoValue = Convert.ToUInt32(Console.ReadLine());
+            byte[] fileBytes = new byte[fileStream.Length];
+            fileStream.Read(fileBytes, 0, fileBytes.Length);
+            long numOfFiles = fileStream.Length / 0x118;
+            int index = 0;
+            for (long i = 0; i < fileBytes.Length; i += 0x118)
+            {
+                index++;
+                uint tempVal = BitConverter.ToUInt32(fileBytes, (int)i);
+                if (Convert.ToUInt32(InfoValue) == tempVal)
+                {
+                    Console.WriteLine(index);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
